f04d0c6ba41d4a02a82522fcd23dd8f4c3eb671a
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-06-22 20:04:45 -0300

    Post Msg v1

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let msg = {
-	from: "nome do usuário",
-	to: "destinatário",
-	text: "mensagem digitada",
-	type: "message"
+const nome = prompt("Qual seu nome de usuário?");
+
+function send() {
+  let msgText = document.querySelector("input").value;
+  let msg = {
+    from: nome,
+    to: "Todos",
+    text: msgText,
+    type: "message",
+  };
+  const sendMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
+  //sendMsg.catch(newName);
 }
 
-function send(msg){
-  axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', msg);
+function getChat() {
+  const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+  promise.then(renderChat);
 }
-const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-promise.then(processPromise);
 
-function processPromise(serverData) {
-	console.log(serverData.data);
+function renderChat(serverData) {
+  console.log(serverData.data);
+  /*
+  let chatFrom = from;
+  let chatTo = ;
+  let chatText = ;
+  let chatType = ;
+  const chatMsg = {chatFrom, chatTo, chatText, chatType};
+  for (n = 0; n < serverData; n++){
+  
+  }*/
 }
