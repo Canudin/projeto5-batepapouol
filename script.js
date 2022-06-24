c60b21cb6d1e89635afa9b6ad6cda6e6ef2333d5
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-06-24 15:01:44 -0300

    login funcionando v1

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-const nome = prompt("Qual seu nome de usuário?");
+let user = "";
+let userObj = { name: user };
+const chatRefresh = setInterval(getChat, 100);
+const statusRefresh = setInterval(online, 5000);
 
+setTimeout(login, 2000);
+chatRefresh;
+
+function login() {
+  user = prompt("Digite seu usuário");
+  userObj = { name: user };
+  const sendUser = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userObj);
+  console.log(userObj);
+  sendUser.then(sendStatus);
+  sendUser.catch(usedName);
+}
+
+function sendStatus() {
+  console.log("enviei login");
+  statusRefresh;
+}
+function online() {
+  const online = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userObj);
+  //console.log(userObj);
+  console.log("online");
+  online.catch(disconnected);
+}
+function usedName() {
+  console.log("erro login");
+  alert("Nome já está em uso");
+  login();
+}
+function disconnected() {
+  clearInterval(statusRefresh);
+  console.log("erro conexão");
+  alert("Você foi desconectado");
+  login();
+}
 function send() {
   let msgText = document.querySelector("input").value;
   let msg = {
-    from: nome,
+    from: user,
     to: "Todos",
     text: msgText,
     type: "message",
   };
-  const sendMsg = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
-  //sendMsg.catch(newName);
+  console.log(msg);
+  axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", msg);
+  document.querySelector("input").value = "";
 }
 
 function getChat() {
@@ -17,15 +54,14 @@ function getChat() {
   promise.then(renderChat);
 }
 
+let allchat = document.querySelector(".chat");
 function renderChat(serverData) {
-  console.log(serverData.data);
-  /*
-  let chatFrom = from;
-  let chatTo = ;
-  let chatText = ;
-  let chatType = ;
+  let chatFrom = "";
+  let chatTo = "";
+  let chatText = "";
+  let chatType = "";
   const chatMsg = {chatFrom, chatTo, chatText, chatType};
-  for (n = 0; n < serverData; n++){
-  
+  /*for (n=0; n<serverData; n++){
+
   }*/
 }
